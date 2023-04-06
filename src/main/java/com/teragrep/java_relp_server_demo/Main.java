c63c370fd1cf69d0c3f99b6698459fcb7121f603
eemhu <125959687+eemhu@users.noreply.github.com>
2023-04-06 10:32:59 +0300

    Add support for TLS demo server (#3)
    * initial commit for tls-demo branch
    
    * Working TLS demo server. Can be enabled with "tls=true" command line argument.
    
    Now port also can be changed with "port=1234" argument. Defaults to 1601.

--- src/main/java/com/teragrep/java_relp_server_demo/Main.java
+++ src/main/java/com/teragrep/java_relp_server_demo/Main.java
@@ -1,21 +1,52 @@
 package com.teragrep.java_relp_server_demo;
 
+import com.teragrep.rlp_01.SSLContextFactory;
 import com.teragrep.rlp_03.FrameProcessor;
 import com.teragrep.rlp_03.Server;
 import com.teragrep.rlp_03.SyslogFrameProcessor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLEngine;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.security.GeneralSecurityException;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 class Main {
-
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) throws IOException,
+            InterruptedException, GeneralSecurityException {
+        int port = 1601;
+        boolean tlsMode = false;
+
+        for (String arg : args) {
+            if (arg.startsWith("port=")) {
+                port = Integer.parseInt(arg.substring(arg.indexOf('=')+1));
+                LOGGER.info("Got port parameter: " + port);
+            }
+            else if (arg.startsWith("tls=")) {
+                String value = arg.substring(arg.indexOf('=')+1);
+                if (value.equalsIgnoreCase("true")) {
+                    tlsMode = true;
+                    LOGGER.info("Got TLS mode parameter. Enabling TLS mode.");
+                }
+            }
+        }
+
+        if (tlsMode) {
+            tlsServer(port);
+        } else {
+            plainServer(port);
+        }
+    }
+
+    private static void plainServer(int port) throws IOException,
             InterruptedException {
+        LOGGER.info("plain server on port " + port);
         Consumer<byte[]> byteConsumer = bytes -> {
             String message = new String(bytes, StandardCharsets.UTF_8);
             LOGGER.info(message);
@@ -24,9 +55,45 @@ class Main {
         FrameProcessor syslogFrameProcessor =
                 new SyslogFrameProcessor(byteConsumer);
 
-        Server relpServer = new Server(1601, syslogFrameProcessor);
+        Server relpServer = new Server(port, syslogFrameProcessor);
 
         relpServer.start();
+
+        Thread.sleep(Long.MAX_VALUE);
+    }
+
+    private static void tlsServer(int port) throws
+            InterruptedException, GeneralSecurityException {
+        LOGGER.info("tls server on port " + port);
+        Consumer<byte[]> byteConsumer = bytes -> {
+            String message = new String(bytes, StandardCharsets.UTF_8);
+            LOGGER.info(message);
+        };
+
+        FrameProcessor syslogFrameProcessor =
+                new SyslogFrameProcessor(byteConsumer);
+
+        SSLContext sslContext;
+        try {
+            sslContext = SSLContextFactory.authenticatedContext(
+                    "src/main/resources/keystore-server.jks", "changeit", "TLSv1.3");
+        } catch (IOException e) {
+            throw new RuntimeException("SSL.demoContext Error: " + e);
+        }
+
+        Function<SSLContext, SSLEngine> sslEngineFunction = sslCtx -> {
+            SSLEngine sslEngine = sslCtx.createSSLEngine();
+            sslEngine.setUseClientMode(false);
+            return sslEngine;
+        };
+
+        Server relpServer = new Server(port, syslogFrameProcessor, sslContext, sslEngineFunction);
+
+        try {
+            relpServer.start();
+        } catch (IOException e) {
+            throw new RuntimeException("RelpServer.Start error: " + e);
+        }
 
         Thread.sleep(Long.MAX_VALUE);
     }
